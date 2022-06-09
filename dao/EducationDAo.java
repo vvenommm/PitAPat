f2fdd21e4da1bd2d171d212b035a7ceb7b2e56a4
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-09 20:49:51 +0900

    Create EducationDAo.java

---
+++ dao/EducationDAo.java
@@ -0,0 +1,25 @@
+package kr.or.ddit.pitapet.education.dao;
+
+
+import java.sql.SQLException;
+import java.util.List;
+
+import kr.or.ddit.pitapet.vo.EducationVO;
+
+public interface EducationDAO {
+	
+	//모든 오프라인 강좌 목록
+	public List<EducationVO> getAllEdu() throws SQLException;
+	
+	//오프라인 강좌 등록 신청
+	
+	
+	//오프라인 강좌 수정
+	
+	
+	//오프라인 강좌 삭제 신청
+	
+	
+	
+	
+}
