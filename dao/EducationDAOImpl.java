7bc38a1f1786faf0fb594e0dc8b384cf029c3e2f
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-11 01:46:52 +0900

    Update EducationDAOImpl.java

--- dao/EducationDAOImpl.java
+++ dao/EducationDAOImpl.java
@@ -10,8 +10,10 @@ import kr.or.ddit.pitapet.vo.EducationVO;
 import kr.or.ddit.util.SqlMapClientFactory;
 
 public class EducationDAOImpl implements EducationDAO {
+	
 	private static EducationDAOImpl dao;
 	private EducationDAOImpl() {}
+	
 	public static EducationDAOImpl getInstance() {
 		if(dao == null) dao = new EducationDAOImpl();
 		return dao;
@@ -26,8 +28,7 @@ public class EducationDAOImpl implements EducationDAO {
 	//1. 모든 오프라인 강좌 목록 조회
 	@Override
 	public List<EducationVO> getAllEdu() throws SQLException {
-		
-		return null;
+		return smc.queryForList("edu.getAllEdu");
 	}
 	
 	
@@ -37,8 +38,7 @@ public class EducationDAOImpl implements EducationDAO {
 	//2. 오프라인 강좌 상세 조회
 	@Override
 	public EducationVO getEduInfo(int edu_no) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		return (EducationVO) smc.queryForObject("edu.getEduInfo", edu_no);
 	}
 	
 	
@@ -48,8 +48,7 @@ public class EducationDAOImpl implements EducationDAO {
 	//3. 오프라인 강좌 신청 시 강아지 정보 불러오기
 	@Override
 	public DogVO getDogInfo(String mem_id) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		return (DogVO) smc.queryForObject("edu.getDogInfo", mem_id);
 	}
 	
 	
@@ -59,8 +58,7 @@ public class EducationDAOImpl implements EducationDAO {
 	//4. 오프라인 강좌 신청 시 강아지 정보 입력
 	@Override
 	public int insertDogInfo(DogVO vo) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		return (int) smc.insert("edu.insertDogInfo", vo);
 	}
 	
 	
@@ -70,8 +68,7 @@ public class EducationDAOImpl implements EducationDAO {
 	//5. 오프라인 강좌 신청 시 강아지 정보 수정된거 있으면 update
 	@Override
 	public int updateDogInfo(DogVO vo) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		return smc.update("edu.updateDogInfo", vo);
 	}
 	
 	
@@ -87,8 +84,7 @@ public class EducationDAOImpl implements EducationDAO {
 	//7. 훈련사가 오프라인 강좌 등록 신청(INSERT)
 	@Override
 	public int insertEdu(EducationVO vo) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		return (int) smc.insert("edu.insertEdu", vo);
 	}
 
 	
@@ -98,8 +94,7 @@ public class EducationDAOImpl implements EducationDAO {
 	//8. 신청된 오프라인 강좌 삭제 신청, 관리자의 승인/비승인(UPDATE) - status에 넣을 String이랑 edu_no
 	@Override
 	public int appInsEdu(EducationVO vo) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		return smc.update("edu.appInsEdu", vo);
 	}
 	
 	
@@ -109,8 +104,7 @@ public class EducationDAOImpl implements EducationDAO {
 	//9. 오프라인 강좌 수정
 	@Override
 	public int modiEdu(EducationVO vo) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		return smc.update("edu.modiEdu", vo);
 	}
 	
 	
