6a8dcc6612712277b29b53d6ac15876632ca51b3
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-10 22:57:34 +0900

    Update CartDAO.java

--- dao/CartDAO.java
+++ dao/CartDAO.java
@@ -1,11 +1,16 @@
 package kr.or.ddit.pitapet.payment.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import kr.or.ddit.pitapet.vo.PaymentVO;
+
 public interface CartDAO {
 
 	//장바구니////////////////////////////////////////////
 	
-	//결제하지 않은 모든 상품 출력
-	
+	//결제하지 않은 모든 상품 출력 = 현재 담은 모든 상품
+//	public List<PaymentVO> inCart() throws SQLException;
 	
 	//결제한 모든 상품 출력
 	
@@ -23,5 +28,8 @@ public interface CartDAO {
 	
 	
 	//내 모든 결제 내역 목록에서 특정 결제 내역 조회할 때 상세 내역 정보 출력
+	
+	
+	//결제 취소 -> 결제여부가 취소로 바뀜
 	
 }
