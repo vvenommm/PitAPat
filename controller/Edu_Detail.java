3953e6eb84e99a1df1c9115f4ce960d52abd203f
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-12 01:25:08 +0900

    Update Edu_Detail.java

--- controller/Edu_Detail.java
+++ controller/Edu_Detail.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.or.ddit.pitapet.education.service.EducationService;
+import kr.or.ddit.pitapet.education.service.EducationServiceImpl;
+import kr.or.ddit.pitapet.vo.EducationVO;
+
 @WebServlet("/Edu_Detail.do")
 public class Edu_Detail extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -18,9 +22,19 @@ public class Edu_Detail extends HttpServlet {
 				response.setCharacterEncoding("utf-8");
 				response.setContentType("text/html; charset=utf-8");
 				
+				//edu_main.jsp에서 클릭한 교육글번호 값 변수에 담기
+				int edu_no = Integer.parseInt(request.getParameter("edu_no"));
+				System.out.println(edu_no);
 				
+				//서비스 객체 생성
+				EducationService service = EducationServiceImpl.getInstance();
 				
+				//결과값 받기 위한 객체 생성 후 값 받기
+				EducationVO eduOne = service.getEduInfo(edu_no);
+				System.out.println(eduOne);
 				
+				//결과값 저장한 객체 request에 저장
+				request.setAttribute("eduOne", eduOne);
 				
 				//view페이지로 이동
 				request.getRequestDispatcher("WEB-INF/view.education/edu_detail.jsp").forward(request, response);
