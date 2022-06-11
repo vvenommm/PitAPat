82a419cc354771d8b1ecc31adec7f3163c530638
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-11 23:51:00 +0900

    Update Edu_Main.java

--- controller/Edu_Main.java
+++ controller/Edu_Main.java
@@ -1,6 +1,7 @@
 package kr.or.ddit.pitapet.education.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -18,7 +19,7 @@ public class Edu_Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO 메인에서 유치원 클릭하면 나오는 메인 jsp로 이동하기
+		// TODO 메인에서 유치원 클릭하면 나오는 edu_main.jsp로 이동하기
 		
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
@@ -35,20 +36,29 @@ public class Edu_Main extends HttpServlet {
 		//요청 시 전송 데이터 받기 - (ajax로 넘겨받기)
 				
 		//service 객체 얻어오기
-		EducationService service = EducationServiceImpl.getInstance();
+//		EducationService service = EducationServiceImpl.getInstance();
 		
 		//결과값 받을 객체 생성 후 servlet 메소드 호출해서 결과값 받기
-		List<EducationVO> eduList = service.getAllEdu();
+//		List<EducationVO> eduList = service.getAllEdu();
+		List<EducationVO> eduList = new ArrayList<EducationVO>();
+		for(int i = 0; i < 5; i++) {
+			EducationVO vo = new EducationVO();
+			vo.setEdu_no("" + i + "");
+			vo.setEdu_title("강아지와 산책하는 법");
+			vo.setEmp_name("훈련사 후후후");
+			vo.setEdu_fee(29000);
+			eduList.add(vo);
+		}
+		
 				
 		//결과 값 받은 객체 request에 저장
-//		request.setAttribute("eduList", eduList);
+		request.setAttribute("eduList", eduList);
 		
 		//view페이지로 이동
 		request.getRequestDispatcher("WEB-INF/view.education/edu_main.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
