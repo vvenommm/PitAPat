ff4febcf5af90147a56168953befc5c36dd5558b
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-12 22:01:02 +0900

    Update Edu_Detail.java

--- controller/Edu_Detail.java
+++ controller/Edu_Detail.java
@@ -16,11 +16,6 @@ public class Edu_Detail extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO edu_main.jsp에서 강의 목록 중 하나 누르면 상세 글로 가도록
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
@@ -30,7 +25,7 @@ public class Edu_Detail extends HttpServlet {
 		int edu_no = Integer.parseInt(request.getParameter("edu_no"));
 		System.out.println(edu_no);
 		
-		//서비스 객체 생성
+		//서비스 객체 생성 ---- 여기서 문제..ㅠㅠㅠ
 		EducationService service = EducationServiceImpl.getInstance();
 		
 		//결과값 받기 위한 객체 생성 후 값 받기
@@ -42,6 +37,11 @@ public class Edu_Detail extends HttpServlet {
 		
 		//view페이지로 이동
 		request.getRequestDispatcher("WEB-INF/view.education/edu_detail.jsp").forward(request, response);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 	}
 
 }
