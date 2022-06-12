9442bd4f2506b64797a7bac0d5f665b0e03230c1
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-12 23:56:49 +0900

    Update Edu_Insert.java

--- controller/Edu_Insert.java
+++ controller/Edu_Insert.java
@@ -1,30 +1,51 @@
-package kr.or.ddit.pitapet.education.controller;
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="shortcut icon" type="image/x-icon" href="../images/logo.png"/>
+<title>피터펫(PIT-A-PET)</title>
+</head>
+<body>
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+<form action="/PitAPet/Edu_Insert.do" method="post">
+	<label>교육 제목</label>
+	<input type='text' id='edu_title' name='edu_title' class="eduInsert">
 
-@WebServlet("/Edu_Insert.do")
-public class Edu_Insert extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO edu_main.jsp에서 강의 등록 누르면 edu_insert.jsp로 이동시키는 메소드
-		
-		request.setCharacterEncoding("utf-8");
-		response.setCharacterEncoding("utf-8");
-		response.setContentType("text/html; charset=utf-8");
-		
-		//edu_insert.jsp로 이동
-		request.getRequestDispatcher("WEB-INF/view.education/edu_insert.jsp").forward(request, response);
-	}
+	<label>교육 내용</label>
+	<input type='text' id='edu_title' name='edu_title' class="eduInsert">
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO edu_insert.jsp에서 강의 등록하기
-		doGet(request, response);
-	}
+	<label>교육 일자</label>
+	<input type='text' id='edu_title' name='edu_title' class="eduInsert">
 
-}
+	<label>교육 장소</label>
+	<select class="eduInsert">
+		<option value="피터펫 운동장">피터펫 운동장</option>
+		<option value="피터펫 강의실 101호">피터펫 강의실 101호</option>
+		<option value="피터펫 강의실 102호">피터펫 강의실 102호</option>
+		<option value="피터펫 강의실 103호">피터펫 강의실 103호</option>
+		<option value="피터펫 출장 방문">출장 방문</option>
+	</select>
+	<input type='text' id='edu_title' name='edu_title' class="eduInsert">
+
+	<label>교육 시간</label>
+	<input type='text' id='edu_title' name='edu_title' class="eduInsert">
+
+	<label>교육 일자</label>
+	<input type='text' id='edu_title' name='edu_title' class="eduInsert">
+
+	<label>교육 일자</label>
+	<input type='text' id='edu_title' name='edu_title' class="eduInsert">
+
+
+
+</form>
+
+
+
+
+
+
+</body>
+</html>
