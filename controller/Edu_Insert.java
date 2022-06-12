16842d1b9fffbcd526589d8c33a833eef921bb4d
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-12 21:24:02 +0900

    Create Edu_Insert.java

---
+++ controller/Edu_Insert.java
@@ -0,0 +1,24 @@
+package kr.or.ddit.pitapet.education.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Edu_Insert.do")
+public class Edu_Insert extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO edu_main.jsp에서 강의 등록 누르면 edu_insert.jsp로 이동시키는 메소드
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO edu_insert.jsp에서 강의 등록하기
+		doGet(request, response);
+	}
+
+}
