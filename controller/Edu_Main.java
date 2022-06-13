611a64c80f18d544172a460a5b18f543f98a6e07
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-13 21:05:38 +0900

    Update Edu_Main.java

--- controller/Edu_Main.java
+++ controller/Edu_Main.java
@@ -36,24 +36,26 @@ public class Edu_Main extends HttpServlet {
 		//요청 시 전송 데이터 받기 - (ajax로 넘겨받기)
 				
 		//service 객체 얻어오기
-//		EducationService service = EducationServiceImpl.getInstance();
+		EducationService service = EducationServiceImpl.getInstance();
 		
 		//결과값 받을 객체 생성 후 servlet 메소드 호출해서 결과값 받기
-//		List<EducationVO> eduList = service.getAllEdu();
-		List<EducationVO> eduList = new ArrayList<EducationVO>();
-		for(int i = 0; i < 5; i++) {
-			EducationVO vo = new EducationVO();
-			vo.setEdu_no(i);
-			vo.setEdu_title("강아지와 산책하는 법");
-			vo.setEmp_name("훈련사 후후후");
-			vo.setEdu_content("강아지와 산책하는 법을 알려드려요!");
-			vo.setEdu_date("22년 06월 11일 목요일");
-			vo.setEdu_time("4시");
-			vo.setEdu_place("피터펫 훈련소");
-			vo.setEdu_price("29,000");
-			vo.setEdu_limit(10);
-			eduList.add(vo);
-		}
+		List<EducationVO> eduList = service.getAllEdu();
+//		List<EducationVO> eduList = new ArrayList<EducationVO>();
+//		for(int i = 11; i < 21; i++) {
+//			EducationVO vo = new EducationVO();
+//			vo.setEdu_no(i);
+//			vo.setEdu_title("강아지와 산책하는 법");
+//			vo.setEmp_name("훈련사 후후후");
+//			vo.setEdu_content("강아지와 산책하는 법을 알려드려요!");
+//			vo.setEdu_date("22년 06월 11일 목요일");
+//			vo.setEdu_time("4시");
+//			vo.setEdu_place("피터펫 훈련소");
+//			vo.setEdu_price("29,000");
+//			vo.setEdu_limit(10);
+//			eduList.add(vo);
+//		}
+		
+		System.out.println("edu main. java");
 		
 				
 		//결과 값 받은 객체 request에 저장
