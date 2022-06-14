45108ca2e0b55976beea063c3105564907eb9231
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-14 11:15:37 +0900

    Update ClassDAO.java

--- dao/ClassDAO.java
+++ dao/ClassDAO.java
@@ -14,13 +14,48 @@ public interface ClassDAO {
 
 	
 	// 1. 모든 온라인 강좌 목록 조회
+	/**
+	 * 모든 온라인 강좌 목록 조회
+	 * @return 모든 강의를 담은 List<ClassVO>
+	 * @throws SQLException
+	 */
 	public List<ClassVO> getAllClass() throws SQLException;
 
 	
 	//////////////////////////////////////////////////////////////////
 
 	
+	//1-1. cls_main.jsp에서 모든 패키지 목록 조회
+	/**
+	 * cls_main.jsp에서 모든 패키지 목록 조회
+	 * @return 모든 패키지를 담은 List<ClassVO>
+	 * @throws SQLException
+	 */
+	public List<ClassVO> getAllPack() throws SQLException;
+	
+	
+	//////////////////////////////////////////////////////////////////
+
+	
+	//1-2. cls_main.jsp에서 모든 패키지 목록에서 각 패키지의 총 강의 수 조회
+	/**
+	 * cls_main.jsp에서 모든 패키지 목록에서 각 패키지의 총 강의 수 조회
+	 * @return 각 패키지 당 강의 수를 담은 List<ClassVO>
+	 * @throws SQLException
+	 */
+	public List<ClassVO> getClsCount() throws SQLException;
+	
+	
+	//////////////////////////////////////////////////////////////////
+
+	
 	// 2. 온라인 강좌 상세 조회
+	/**
+	 * 온라인 강좌 상세 조회
+	 * @param cls_no
+	 * @return 선택한 강좌 정보를 담은 ClassVO
+	 * @throws SQLException
+	 */
 	public ClassVO getClsInfo (int cls_no) throws SQLException;
 	
 	
@@ -30,6 +65,12 @@ public interface ClassDAO {
 	
 	
 	// 3. 훈련사가 온라인 강좌 등록 신청
+	/**
+	 * 훈련사가 온라인 강좌 등록 신청
+	 * @param vo 훈련사가 등록하고자 하는 강좌 정보를 담은 객체
+	 * @return 성공 : 1, 실패 : 0
+	 * @throws SQLException
+	 */
 	public int insertCls (ClassVO vo) throws SQLException;
 
 	
@@ -37,6 +78,12 @@ public interface ClassDAO {
 	
 	
 	// 4. 신청된 온라인 강좌 승인/비승인 (등록, 승인, 취소, 삭제)
+	/**
+	 * 신청된 온라인 강좌 승인/비승인 (등록, 승인, 취소, 삭제)
+	 * @param vo 훈련사/관리자가 등록, 승인, 취소, 삭제하고자 하는 강좌 정보를 담은 객체
+	 * @return 성공 : 1, 실패 : 0
+	 * @throws SQLException
+	 */
 	public int appInsCls (ClassVO vo) throws SQLException;
 	
 	
@@ -44,6 +91,12 @@ public interface ClassDAO {
 
 	
 	// 5. 온라인 강좌 수정
+	/**
+	 * 온라인 강좌 수정
+	 * @param vo 훈련사가 자기 강좌를 수정하고 싶을 때 수정 정보를 담은 객체
+	 * @return 성공 : 1, 실패 : 0
+	 * @throws SQLException
+	 */
 	public int modiCls (ClassVO vo) throws SQLException;
 
 	
