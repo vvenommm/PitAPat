dd994b62ad9bb8cebeda935226a1f64caa8dd540
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-19 17:38:09 +0900

    Update Edu_myEdu.java

--- controller/Edu_myEdu.java
+++ controller/Edu_myEdu.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import kr.or.ddit.pitapet.education.service.EducationService;
 import kr.or.ddit.pitapet.education.service.EducationServiceImpl;
@@ -28,32 +29,19 @@ public class Edu_myEdu extends HttpServlet {
 		response.setContentType("application/json; charset=utf-8");
 		
 		//값 받기
-		String emp_id = request.getParameter("mem_id");
-		
-		//Gson 객체 생성
-//		Gson gson = new Gson();
+		HttpSession session = request.getSession();
+		String id = (String)session.getAttribute("id");
 		
 		// 서비스 객체 생성
 		EducationService service = EducationServiceImpl.getInstance();
 
 		// 서비스로 값 가져와서 받기
-		List<EducationVO> myEduList = service.getMyEdu(emp_id);
+		List<EducationVO> myEduList = service.getMyEdu(id);
 		
-		System.out.println(myEduList); //삭제하기
-		
-		//응답용 데이터를 JSON 문자열로 변환 & JSON 문자열이 저장될 변수 선언
-//		String jsonData = gson.toJson(myEduList);
-		
-		
-		System.out.println(myEduList); //삭제하기
+		System.out.println("edu_myEdu.java의 myEduList :  " + myEduList); //삭제하기
 
 		// 값 저장하기
 		request.setAttribute("myEduList", myEduList);
-		
-		//변환된 JSON 문자열을 응답 데이터로 전송
-//		PrintWriter out = response.getWriter();
-//		out.write(jsonData);
-//		response.flushBuffer();
 		
 		// view페이지로 이동
 		request.getRequestDispatcher("WEB-INF/view.education/edu_myEdu.jsp").forward(request, response);
